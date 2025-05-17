129c58dc826eab16df9a01c8161c5b85ac850554
Ricardo Almeida <ric.almeida@gmail.com>
2025-05-17 17:41:18 +0100

    Add error example

--- src/main/react-js/app.jsx
+++ src/main/react-js/app.jsx
@@ -3,5 +3,13 @@ import "./app.scss"
 import React from 'react';
 
 export default function App(props) {
-    return <div>Hello World!</div>
+    return <select>
+        <option
+            value="123"
+            className="option-with-color"
+            data-color="#abcdef">
+        With Color
+        </option>
+        <option value="456">No Color</option>
+    </select>
 }
